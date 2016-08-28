96e0a105ef7a534e31cf3884faba0ca8b5795b0e
zuyuan <894918097@qq.com>
2016-08-28 11:14:27 +0800

    bufferconcat

--- 8.http/server.js
+++ 8.http/server.js
@@ -1,17 +1,17 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
-//引用我们的第三方包 mime
 http.createServer(function (req,res) {
     var urlObj = require('url').parse(req.url,true);
     var pathname = urlObj.pathname;
     if(pathname =='/'){
         res.setHeader('Content-type','text/html;charset=utf8');
         fs.createReadStream('./1.html').pipe(res);
+    }else if(pathname=='/clock'){
+        res.end(new Date().toLocaleString())
     }else{
         var exists = fs.existsSync('.'+pathname);
         if(exists){
-            //查看一下当前的pathname路径是什么类型的
             res.setHeader('Content-type',require('mime').lookup(pathname)+';charset=utf8');
             fs.createReadStream('.'+pathname).pipe(res);
         }else{
@@ -22,17 +22,3 @@ http.createServer(function (req,res) {
         }
     }
 }).listen(3000);
-
-
-
-
-/*
-else if(pathname =='/lib/index.css'){
-    res.setHeader('Content-type','text/css;charset=utf8');
-    fs.createReadStream('./lib/index.css').pipe(res);
-}else if(pathname =='/lib/index.js'){
-    res.setHeader('Content-type','application/x-javascript;charset=utf8');
-    fs.createReadStream('./lib/index.js').pipe(res);
-}else{
-    res.end('结束')
-}*/
